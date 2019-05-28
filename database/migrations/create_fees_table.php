5f8fa60426c35d2b03f19c176471b8a527256b15
Ian Odundo <thisian13@gmail.com>
2019-05-28 20:35:04 +0300

    Initial commit

---
+++ database/migrations/create_fees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            $table->bigIncrements('transaction_id')->unique();
+            $table->string('student_no');
+            $table->date('date');
+            $table->double('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fees');
+    }
+}
