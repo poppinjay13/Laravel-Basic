fbbba30a594f17e629084a9798af0e0ab6688e04
Ian Odundo <thisian13@gmail.com>
2019-05-28 21:26:40 +0300

    blades

---
+++ resources/views/100446/student.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>100446 - HOME</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href= {{ asset('css/bootstrap.min.css') }} rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+
+  <!-- Page Content -->
+<center>
+<table style="height: 100vh;">
+	<tbody>
+		<tr>
+			<td><a href="/student" type="button" class="btn btn-primary btn-lg">Register User</a></td>
+			<td><a href="/fees" type="button" class="btn btn-primary btn-lg">Pay Fees</a></td>
+		</tr>
+	</tbody>
+</table>
+</center>
+  <!-- Bootstrap core JavaScript -->
+  <script src= {{ asset('js/jquery/jquery.slim.min.js') }}></script>
+  <script src= {{ asset('js/bootstrap.bundle.min.js') }}></script>
+
+</body>
+
+</html>
